1ced154f85942ea88fe45ddd874a065f86db8949
Julien <julien@localhost.localdomain>
2020-09-29 15:27:27 +0200

    lsls

--- CPOOL1/push_that.sh
+++ CPOOL1/push_that.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add -A
 read -s message
-git commit -m $message
+git commit -m "$message" 
 git pull
 git push -u origin master
--- push_that.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git add -A
-read -s message
-git commit -m "$message" 
-git pull
-git push -u origin master
